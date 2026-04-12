b533aabf867a19b9f4ee514a994ae8f5dceb148f
Mohammad Shakhawat Hossain Shoikat <136487811+Md-Shoikat@users.noreply.github.com>
2026-04-12 14:16:20 +0600

    lab Task 1 -A

---
+++ Final_Lab_Task_1/Lab_Task_1/A/handler.php
@@ -0,0 +1,22 @@
+
+<?php
+$name = $_POST["name"];
+?>
+
+
+
+
+
+
+
+<!DOCTYPE html>
+<html>
+<body>
+
+<form method="post" >
+    Name: <input type="text" name="name">
+    <input type="submit" value="Submit">
+</form>
+
+</body>
+</html>
