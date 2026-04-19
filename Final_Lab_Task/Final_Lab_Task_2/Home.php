b1633b99a501e1b127cd1ee322a3efaaf20c544a
Mohammad Shakhawat Hossain Shoikat <136487811+Md-Shoikat@users.noreply.github.com>
2026-04-19 14:14:46 +0600

    Final lab Task 2

---
+++ Final_Lab_Task/Final_Lab_Task_2/Home.php
@@ -0,0 +1,17 @@
+<?php
+session_start();
+?>
+<!DOCTYPE html>
+<html>
+<head><title>Home</title></head>
+<body>
+
+<h2>Welcome to X Company</h2>
+
+<a href="home.php">Home</a><br>
+<a href="login.php">Login</a><br>
+<a href="registration.php">Registration</a><br>
+
+
+</body>
+</html>
